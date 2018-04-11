0d40e16a92449407bdb2ca5b406be0049739ccd8
Annapurna13 <yoavgecht@gmail.com>
2018-04-11 13:27:19 +0300

    updates

--- src/actions/data-actions.js
+++ src/actions/data-actions.js
@@ -2,12 +2,24 @@ import axios from 'axios';
 
 const saveUrl = '/api/save-ticker-data';
 
+export function setStockData(stockData) {
+  return dispatch=> {
+    return dispatch({
+      type: 'SET_STOCKDATA',
+      stockData,
+      })
+  }
+}
+
+
 export function saveStockData(stockData) {
   return dispatch=> {
     return dispatch({
       type: 'SAVE_STOCKDATA',
+      stockData,
       payload: axios.post(saveUrl, stockData)
-              .then(res => res.json())
+        .then( response => response.json())    
+        .then(data => dispatch(setStockData(data))) 
       })
   }
 }
