d4c6f68e9169a9640454bd635b3480b609f0aa9f
Annapurna13 <yoavgecht@gmail.com>
2018-04-11 12:21:56 +0300

    updates

--- src/components/searched/SearchesListComponent.js
+++ src/components/searched/SearchesListComponent.js
@@ -2,13 +2,17 @@ import React from 'react';
 import { ListGroup,  ListGroupItem} from 'react-bootstrap';
 
 const SearchedListComponent = (props) => (
-<ListGroup>
-    {props.searchHistory.map((item, i) =>
-         <ListGroupItem>
-            item
-         </ListGroupItem>
-    )}
-</ListGroup>  
+    <div>
+    <label>Search history:</label>
+    <ListGroup>
+        
+            {props.searchHistory.map((item, i) =>
+            <ListGroupItem>
+                {item}
+            </ListGroupItem>
+        )}
+    </ListGroup>  
+    </div>
 );
 
 
