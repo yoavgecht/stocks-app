49654f36dfb7aa5bbb2e761c79c9419909ca9159
sureshHARDIYA <itsmeskm99@gmail.com>
2018-04-12 17:37:38 +0545

    change remove icon to trash icon

--- src/components/searched/SearchesListComponent.js
+++ src/components/searched/SearchesListComponent.js
@@ -1,20 +1,28 @@
-import React from 'react';
-import { ListGroup,  ListGroupItem} from 'react-bootstrap';
+import React from "react";
+import { ListGroup, ListGroupItem } from "react-bootstrap";
 
-const SearchedListComponent = (props) => (
-    <div>
+const SearchedListComponent = props => (
+  <div>
     <label>Search history:</label>
     <ListGroup>
-        {props.searchHistory.map((item, i) =>
-        <ListGroupItem item={item} onClick={() => props.showSearchedData(item._id)}>
-            <span><strong>{item.dataset_code} - {item.start_date}</strong></span> 
-            <span class="glyphicon glyphicon-remove"  onClick={() => props.deleteStock(item._id)}></span> 
+      {props.searchHistory.map((item, i) => (
+        <ListGroupItem
+          item={item}
+          onClick={() => props.showSearchedData(item._id)}
+        >
+          <span>
+            <strong>
+              {item.dataset_code} - {item.start_date}
+            </strong>
+          </span>
+          <span
+            class="glyphicon glyphicon-trash pull-right"
+            onClick={() => props.deleteStock(item._id)}
+          />
         </ListGroupItem>
-        )}
-    </ListGroup>  
-    </div>
+      ))}
+    </ListGroup>
+  </div>
 );
-
-
 
 export default SearchedListComponent;
