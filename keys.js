65b67e807baf3d1ffe4d95ac5fc2ec1652212b93
Annapurna13 <yoavgecht@gmail.com>
2018-04-18 12:21:47 +0300

    updates

--- keys.js
+++ keys.js
@@ -17,3 +17,5 @@ module.exports = {
 // mongodb://<dbuser>:<dbpassword>@ds135179.mlab.com:35179/heroku_8z855b21
 
 // mongodb://heroku_8z855b21:6et084bqf503cjatm8a9kh4vf6@ds135179.mlab.com:35179/heroku_8z855b21
+
+// https://www.quandl.com/api/v3/datasets/WIKI/amzn.json?start_date=2018/04/01&end_date=2018/04/16&collapse=daily&key=Vk29NQzysMurK6sfhDxw
