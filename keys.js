40361919b03e1240dbc2f8b7c05df6394769603e
Annapurna13 <yoavgecht@gmail.com>
2018-04-18 11:17:22 +0300

    updates

---
+++ keys.js
@@ -0,0 +1,19 @@
+module.exports = {
+    quandlApiKey : 'Vk29NQzysMurK6sfhDxw',
+    localDbName  : 'stocksAppDb',
+    dbURI        : 'mongodb://undefined:undefined03@ds135179.mlab.com:35179/heroku_8z855b21'
+}
+
+
+// heroku git:remote -a react-stocks-app
+
+// deploy to heroku
+// add https://github.com/mars/create-react-app-buildpack.git to build packs
+// add Procfile
+// add static.json
+
+// https://github.com/mars/create-react-app-buildpack.gitð
+
+// mongodb://<dbuser>:<dbpassword>@ds135179.mlab.com:35179/heroku_8z855b21
+
+// mongodb://heroku_8z855b21:6et084bqf503cjatm8a9kh4vf6@ds135179.mlab.com:35179/heroku_8z855b21
